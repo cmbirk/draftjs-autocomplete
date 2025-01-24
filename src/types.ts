3778839c54793615469989b00de3c008f77f8ef8
Chris Birk <cmbirk@gmail.com>
2025-01-24 04:34:31 -0600

    fix path aliases

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import React from 'react';
 import { ContentState } from "draft-js";
 
 // Injected by Draft.js
-export type AutocompleteEntryProps = {
+export interface AutocompleteEntryProps {
   children: React.ReactNode;
   contentState: ContentState;
   entityKey: string;
